65c7185e79671041dcbe187201e8829d615145f0
Jonathan Perez <jonathanperezmota@gmail.com>
2018-11-21 19:46:48 -0400

    Initial commit

---
+++ src/main/java/com/friendlyratingmovies/ratingmoviesapp/RatingmoviesappApplication.java
@@ -0,0 +1,12 @@
+package com.friendlyratingmovies.ratingmoviesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingmoviesappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingmoviesappApplication.class, args);
+	}
+}
