a985b2e6b5e11b61b50ab0625f9cde7612780abe
Jonathan Perez <jonathanperezmota@gmail.com>
2018-11-21 22:32:32 -0400

    Adding spring security basic schema, along with movies schema. Populated movies schema.

---
+++ src/main/resources/db/migration/V2__populate.sql
@@ -0,0 +1,19 @@
+insert into movies 
+(title, 
+ synopsis, 
+ release_year
+) 
+values 
+("Venom", 
+"Journalist Eddie Brock is trying to take down Carlton Drake, the notorious and brilliant founder of the Life Foundation. While investigating one of Drake's experiments, Eddie's body merges with the alien Venom -- leaving him with superhuman strength and power. Twisted, dark and fueled by rage, Venom tries to control the new and dangerous abilities that Eddie finds so intoxicating.", 
+2018);
+
+insert into movies 
+(title, 
+ synopsis, 
+ release_year
+) 
+values 
+("Black Panther", 
+"After the death of his father, T'Challa returns home to the African nation of Wakanda to take his rightful place as king. When a powerful enemy suddenly reappears, T'Challa's mettle as king -- and as Black Panther -- gets tested when he's drawn into a conflict that puts the fate of Wakanda and the entire world at risk. Faced with treachery and danger, the young king must rally his allies and release the full power of Black Panther to defeat his foes and secure the safety of his people.", 
+2018);
